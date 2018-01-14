7604f577cf7d5f1036d9036f43f13795b04b2a8f
Mateusz Bargiel <mateuszbargiel@Mateuszs-MacBook-Pro.local>
2018-01-14 12:39:28 +0100

    project reinit

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,18 @@
-const path = require('path');
-
 module.exports = {
-  entry: './src/game.ts',
   devtool: 'inline-source-map',
-  node: {
-    __dirname: false
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
+  entry: './app.ts',
+  output: {
+    filename: './dist/bundle.js'
   },
   resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ]
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: ['.ts', '.tsx', '.js'] // note if using webpack 1 you'd also need a '' in the array as well
   },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+  module: {
+    loaders: [ // loaders will work with webpack 1 or 2; but will be renamed "rules" in future
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.tsx?$/, loader: 'ts-loader' }
+    ]
   }
-};
+}
+  
